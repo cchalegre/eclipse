00c090769fa02046c911b0974f60c112bb408b4b
cchalegre <85136246+cchalegre@users.noreply.github.com>
2021-05-31 18:31:52 -0300

    Add files via upload

---
+++ Hello GitHub/src/br/com/cchalegre/git/HelloGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.com.cchalegre.git;
+
+/**
+ * @author Caio Chalegre
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		
+
+	}
+
+}
